355d0b0d2230cf957a9e203d7c6cf4707b94a251
Albin Mathew <albinmathewtomo@gmail.com>
2026-09-25 17:44:40 +0000

    [LeetCode Sync] Runtime - 1 ms (94.67%), Memory - 61 MB (98.08%)

---
+++ leetcode/0011-container-with-most-water/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
+    while (left < right) {
+        let width = right - left;
+        let h = Math.min(height[left], height[right]);
+        maxWater = Math.max(maxWater, width * h);
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxWater;
+};
