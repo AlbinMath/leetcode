1cc5253ebcc8788b28b51a1f2427eb8df786532f
Albin Mathew <albinmathewtomo@gmail.com>
2026-09-25 18:03:35 +0000

    [LeetCode Sync] Runtime - 3 ms (70.76%), Memory - 57.4 MB (18.81%)

---
+++ leetcode/0020-valid-parentheses/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    for (let ch of s) {
+        if (ch === '(') {
+            stack.push(')');
+        } else if (ch === '[') {
+            stack.push(']');
+        } else if (ch === '{') {
+            stack.push('}');
+        } else {
+            if (stack.length === 0 || stack.pop() !== ch) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
