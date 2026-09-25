5bf71a6af608f57dfc3d9d2380c3f234bd1492b2
Albin Mathew <albinmathewtomo@gmail.com>
2026-09-25 17:39:48 +0000

    [LeetCode Sync] Runtime - 51 ms (59.02%), Memory - 57.4 MB (9.11%)

---
+++ leetcode/0007-reverse-integer/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let sign = x < 0 ? -1 : 1;
+    x = Math.abs(x);
+
+    let rev = 0;
+
+    while (x > 0) {
+        let digit = x % 10;
+        rev = rev * 10 + digit;
+        x = Math.trunc(x / 10);
+    }
+
+    rev *= sign;
+
+    if (rev < -2147483648 || rev > 2147483647) {
+        return 0;
+    }
+
+    return rev;
+};
