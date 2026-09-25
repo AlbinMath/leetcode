09b2326ce7775cd07f756acc1a39d4f6e4b97588
Albin Mathew <albinmathewtomo@gmail.com>
2026-09-25 12:43:39 +0000

    [LeetCode Sync] Runtime - 21 ms (35.59%), Memory - 77.4 MB (67.73%)

---
+++ leetcode/4285-smallest-stable-index-ii/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var firstStableIndex = function(nums, k) {
+    const n = nums.length;
+
+    // Find suffix minimums
+    const suffixMin = new Array(n);
+    suffixMin[n - 1] = nums[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+    }
+
+    // Check from left to right
+    let prefixMax = -Infinity;
+
+    for (let i = 0; i < n; i++) {
+        prefixMax = Math.max(prefixMax, nums[i]);
+
+        if (prefixMax - suffixMin[i] <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+};
