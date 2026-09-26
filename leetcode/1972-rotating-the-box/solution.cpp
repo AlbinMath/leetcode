070014bcbb15476dac880c43b0111eba6f0fdf53
Albin Mathew <albinmathewtomo@gmail.com>
2026-09-26 13:24:28 +0000

    [LeetCode Sync] Runtime - 15 ms (40.20%), Memory - 56.6 MB (90.25%)

---
+++ leetcode/1972-rotating-the-box/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+        // 1. Let stones fall to the right
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                }
+                else if (boxGrid[i][j] == '#') {
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][empty] = '#';
+                    empty--;
+                }
+            }
+        }
+
+        // 2. Rotate 90 degrees clockwise
+        vector<vector<char>> result(n, vector<char>(m));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return result;
+    }
+};
