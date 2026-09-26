5fdf30fc2872eef41a4c155adae413720c280e07
Albin Mathew <albinmathewtomo@gmail.com>
2026-09-26 14:01:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 30.4 MB (65.11%)

---
+++ leetcode/1428-jump-game-iii/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        vector<bool> visited(n, false);
+        queue<int> q;
+
+        q.push(start);
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int i = q.front();
+            q.pop();
+
+            // Reached an index with value 0
+            if (arr[i] == 0)
+                return true;
+
+            // Jump right
+            int right = i + arr[i];
+            if (right < n && !visited[right]) {
+                visited[right] = true;
+                q.push(right);
+            }
+
+            // Jump left
+            int left = i - arr[i];
+            if (left >= 0 && !visited[left]) {
+                visited[left] = true;
+                q.push(left);
+            }
+        }
+
+        return false;
+    }
+};
